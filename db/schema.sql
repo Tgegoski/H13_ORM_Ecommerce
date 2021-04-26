b7bca329d9ac0a165355f60c81b839575dc877b2
Tammy Gegoski <tgegoski@gmail.com>
2021-04-26 10:19:38 -0400

    set up tables in schema file

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,32 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_DB;
+
+CREATE TABLE category(
+    id INT AUTO_INCREMENT NOT NULL,
+    category_name STRING(30) NOT NULL,
+    PRIMARY KEY (id) 
+);
+
+CREATE TABLE product(
+    id INT AUTO_INCREMENT NOT NULL,
+    product_name STRING(30) NOT NULL,
+    price DECIMAL UNSIGNED NOT NULL, 
+    stock INT(10) NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tag(
+    id INT AUTO_INCREMENT NOT NULL,
+    tag_name STRING,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE productTag(
+    id INT AUTO_INCREMENT NOT NULL,
+    product_id INT,
+    tag_id INT,
+    PRIMARY KEY (id)
+);
